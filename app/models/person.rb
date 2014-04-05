e9eb9245c964a90fc841f3e7b62dc2f8b0a8f758
davidakimbo <minheeman@gmail.com>
2014-04-05 15:16:35 -0400

    Finally made birthday work. Silly RoR conventions

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,8 @@ class Person < ActiveRecord::Base
     def full_name
       @full_name = self.first_name + " " + self.last_name
     end
+
+    def birthday
+      @birthday = self.birthdate.to_s
+    end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,7 +4,10 @@ require 'spec_helper'
     it "#name should return the objects full name" do
       tommy = Person.create(:first_name => "Tommy", :last_name => "Threadcraft")
       expect(tommy.full_name).to eq("Tommy Threadcraft")
+    end
 
-
+    it "#birthday should return the objects birthdate" do
+      david = Person.create(:birthdate => "August 31st 1985")
+      expect(david.birthday).to eq("1985-08-31")
     end
   end #Describe Person
